72fb2458e3c5d13515f2a687908e97efef24e805
Petr <petr.kurapov@gmail.com>
2021-02-08 16:09:58 +0300

    Add clean spirv build scripts

---
+++ build_spirv.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+script=$(readlink -f "$0")
+scriptpath=$(dirname "$script")
+
+if [ -z $GFX_BUILD_HOME ]; then
+    echo "GFX_BUILD_HOME unset. Please use export GFX_BUILD_HOME=/your/path"
+    exit 1
+fi
+
+cd $GFX_BUILD_HOME || exit 1
+mkdir -p install/spirv
+mkdir -p build/spirv
+rm -rf install/spirv/*
+
+cd llvm-project || exit 1
+git checkout release/10.x
+cd llvm/project/llvm-spirv
+git checkout llvm_release_100
+
+cd $GFX_BUILD_HOME/build/spirv || exit 1
+# in-tree build
+cmake -DCMAKE_INSTALL_PREFIX=$GFX_BUILD_HOME/install/spirv -DLLVM_ENABLE_PROJECTS="clang" $GFX_BUILD_HOME/llvm-project/llvm/
+make llvm-spirv -j`nproc`
---
+++ get_spirv.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+script=$(readlink -f "$0")
+scriptpath=$(dirname "$script")
+
+if [ -z $GFX_BUILD_HOME ]; then
+    echo "GFX_BUILD_HOME unset. Please use export GFX_BUILD_HOME=/your/path"
+    exit 1
+fi
+
+cd $GFX_BUILD_HOME || exit 1
+
+git clone -b release/10.x https://github.com/llvm/llvm-project llvm-project-for-spirv
+git clone -b llvm_release_100 https://github.com/KhronosGroup/SPIRV-LLVM-Translator llvm-project-for-spirv/llvm/projects/llvm-spirv

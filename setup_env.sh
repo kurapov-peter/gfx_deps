d832dcce1b8f462dedc5aca708b0dfd38e604c39
Petr <petr.kurapov@gmail.com>
2021-02-08 15:02:29 +0300

    Add setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+if [ -z $GFX_BUILD_HOME ]; then
+    echo "GFX_BUILD_HOME unset. Please use export GFX_BUILD_HOME=/your/path"
+    exit 1
+fi
+
+export LD_LIBRARY_PATH=$GFX_BUILD_HOME/install/neo_loader/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$GFX_BUILD_HOME/install/neo/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$GFX_BUILD_HOME/install/igc/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$GFX_BUILD_HOME/install/gmm/lib:$LD_LIBRARY_PATH

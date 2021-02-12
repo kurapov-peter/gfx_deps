cec234f11093a65d240b80b4f70bed2f34ae8911
Petr <petr.kurapov@gmail.com>
2021-02-12 15:34:20 +0300

    Fix spirv path

--- build_spirv.sh
+++ build_spirv.sh
@@ -13,12 +13,15 @@ mkdir -p install/spirv
 mkdir -p build/spirv
 rm -rf install/spirv/*
 
-cd llvm-project || exit 1
+cd $GFX_BUILD_HOME/llvm-project || exit 1
 git checkout release/10.x
-cd llvm/project/llvm-spirv
+cd $GFX_BUILD_HOME/llvm-project/llvm/projects/llvm-spirv || exit 1
 git checkout llvm_release_100
 
 cd $GFX_BUILD_HOME/build/spirv || exit 1
 # in-tree build
 cmake -DCMAKE_INSTALL_PREFIX=$GFX_BUILD_HOME/install/spirv -DLLVM_ENABLE_PROJECTS="clang" $GFX_BUILD_HOME/llvm-project/llvm/
-make llvm-spirv -j`nproc'
+make llvm-spirv -j`nproc`
+
+#cmake -DCMAKE_INSTALL_PREFIX=$GFX_BUILD_HOME/install/spirv -DLLVM_DIR=$GFX_BUILD_HOME/build/llvm/cmake/llvm 
+#make llvm-spirv -j`nproc`

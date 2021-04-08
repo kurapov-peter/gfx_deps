931d086c43fb3b80d9e6c512c9ec07c32578d0c2
Petr <petr.kurapov@gmail.com>
2021-04-08 18:44:54 +0300

    Fix invocation

--- build_env_no_spirv.sh
+++ build_env_no_spirv.sh
@@ -3,8 +3,8 @@
 script=$(readlink -f "$0")
 scriptpath=$(dirname "$script")
 
-$scriptpath/build_igc.sh || exit 1
-$scriptpath/build_gmm.sh || exit 1
-$scriptpath/build_loader.sh || exit 1
-$scriptpath/build_neo.sh || exit 1
+bash $scriptpath/build_igc.sh || exit 1
+bash $scriptpath/build_gmm.sh || exit 1
+bash $scriptpath/build_loader.sh || exit 1
+bash $scriptpath/build_neo.sh || exit 1
 
--- get_env_no_spirv.sh
+++ get_env_no_spirv.sh
@@ -3,7 +3,7 @@
 script=$(readlink -f "$0")
 scriptpath=$(dirname "$script")
 
-$scriptpath/get_loader.sh || exit 1
-$scriptpath/get_neo.sh || exit 1
-$scriptpath/get_igc.sh || exit 1
+bash $scriptpath/get_loader.sh || exit 1
+bash $scriptpath/get_neo.sh || exit 1
+bash $scriptpath/get_igc.sh || exit 1
 

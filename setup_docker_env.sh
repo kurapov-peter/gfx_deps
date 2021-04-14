52032c2e6ae81ac4902479af0941de270ade6ba5
Petr Kurapov <petr.a.kurapov@intel.com>
2021-04-14 19:33:34 +0300

    Update setup_docker_env.sh

--- setup_docker_env.sh
+++ setup_docker_env.sh
@@ -1,6 +1,6 @@
 if [ -z $GFX_BUILD_HOME ]; then
     echo "GFX_BUILD_HOME unset. Please use export GFX_BUILD_HOME=/your/path"
-    exit 1
+    return
 fi
 
 export LD_LIBRARY_PATH=$GFX_BUILD_HOME/neo_loader/lib:$LD_LIBRARY_PATH
